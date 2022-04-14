c3e81067ba0850a9768d31b4254f2f4d10f99158
Daniel Ochoja <danielochoja@yahoo.com>
2022-04-14 16:28:43 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 const nav = document.querySelector("#menu");
+var hide1 = document.getElementsByTagName("button");
+var btnStatus = true; //hides button
 
 document.getElementById("menu-icon").addEventListener("click", () => {
   nav.classList.toggle("show");
+  btnStatus = !btnStatus;
+
+  if (btnStatus == false) {
+    for (let i = 0, max = hide1.length; i < max; i++) {
+      btnStatus = !btnStatus;
+      hide1[i].classList.toggle("none");
+    }
+  } else {
+    for (let i = 0, max = hide1.length; i < max; i++) {
+      btnStatus = !btnStatus;
+      hide1[i].classList.toggle("none");
+    }
+  }
 });
