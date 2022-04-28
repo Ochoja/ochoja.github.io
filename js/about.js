4217a25dcbd010b2ccecd724ac6b8df3c0866f77
Daniel Ochoja <danielochoja@yahoo.com>
2022-04-28 07:32:41 +0100

    updated

--- js/about.js
+++ js/about.js
@@ -1,6 +1,9 @@
 const mission_btn = document.querySelectorAll(".mission_btn");
 const mission_text = document.querySelectorAll(".mission_text");
-console.log(mission_text);
+const card = document.querySelectorAll(".card"); //contact cards
+const popup = document.querySelector(".popup"); //popup_container
+const popup_modal = document.querySelectorAll(".popup_modal"); //popup_modal
+const close_popup = document.querySelectorAll(".close_popup");
 
 for (let i = 0; i < mission_btn.length; i++) {
   mission_btn[i].addEventListener("click", () => {
@@ -11,5 +14,22 @@ for (let i = 0; i < mission_btn.length; i++) {
     }
     mission_btn[i].classList.add("active"); //add active class to clicked button
     mission_text[i].classList.add("show");
+  });
+}
+
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener("click", () => {
+    popup.classList.add("show");
+    popup_modal[i].classList.add("show");
+  });
+}
+
+for (let i = 0; i < close_popup.length; i++) {
+  close_popup[i].addEventListener("click", () => {
+    popup.classList.remove("show");
+
+    for (let i = 0; i < popup_modal.length; i++) {
+      popup_modal[i].classList.remove("show");
+    }
   });
 }
